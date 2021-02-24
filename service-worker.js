d16e0178f3253b72bd19cbde9f05a42d37918dba
Ross <44373734+rossmarinaro@users.noreply.github.com>
2021-02-24 14:27:13 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0f1769e924f1222bd3c494c98797856e.js"
+  "/portfolio/precache-manifest.2482297857aa716ae9044fb7688b55e0.js"
 );
 
 self.addEventListener('message', (event) => {
