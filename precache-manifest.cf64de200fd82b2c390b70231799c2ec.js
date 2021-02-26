9025575e2ce14d11725813cf4ada1308bd4dc365
Ross <44373734+rossmarinaro@users.noreply.github.com>
2021-02-26 18:33:04 -0500

    Add files via upload

---
+++ precache-manifest.cf64de200fd82b2c390b70231799c2ec.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbec9bc5f6d7c1ee2061deb36d7cd6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b4e1e8920ba6ba54bb8b",
+    "url": "/portfolio/static/css/main.9a9cb60c.chunk.css"
+  },
+  {
+    "revision": "6aca149677adf4f12fd7",
+    "url": "/portfolio/static/js/2.ef7f7467.chunk.js"
+  },
+  {
+    "revision": "b4e1e8920ba6ba54bb8b",
+    "url": "/portfolio/static/js/main.1dd07c93.chunk.js"
+  },
+  {
+    "revision": "1a4d47e7058fefdd3b2f",
+    "url": "/portfolio/static/js/runtime-main.fe203139.js"
+  },
+  {
+    "revision": "617ae92ed8b2fdecca9cd779852b8549",
+    "url": "/portfolio/static/media/1.617ae92e.png"
+  },
+  {
+    "revision": "6780da489de471299361109929b8d261",
+    "url": "/portfolio/static/media/1.6780da48.png"
+  },
+  {
+    "revision": "6cd9f44c1f5fa50b86d0990d5794587b",
+    "url": "/portfolio/static/media/1.6cd9f44c.png"
+  },
+  {
+    "revision": "c80924a64283ff08b7ccfb3c5fa3d1e4",
+    "url": "/portfolio/static/media/2.c80924a6.png"
+  },
+  {
+    "revision": "ee397d399d719226336f92c81d2fa6be",
+    "url": "/portfolio/static/media/2.ee397d39.png"
+  },
+  {
+    "revision": "f80feb931bc53e06c358d89dd6149185",
+    "url": "/portfolio/static/media/2.f80feb93.png"
+  },
+  {
+    "revision": "03e0b5eaf9347b43cc602feba4898e41",
+    "url": "/portfolio/static/media/3.03e0b5ea.png"
+  },
+  {
+    "revision": "6d45ab55f36813c8f2ca23a7d110d7c6",
+    "url": "/portfolio/static/media/3.6d45ab55.png"
+  },
+  {
+    "revision": "8a5c15153532532535b452f4dfb6728c",
+    "url": "/portfolio/static/media/3.8a5c1515.png"
+  },
+  {
+    "revision": "b6b2f400c784436d342410808fd4f46d",
+    "url": "/portfolio/static/media/4.b6b2f400.png"
+  },
+  {
+    "revision": "ec5a0031127fbfd12c1691668a74c1db",
+    "url": "/portfolio/static/media/4.ec5a0031.png"
+  },
+  {
+    "revision": "63028b801b021d4f09bfbeac91c1d6ea",
+    "url": "/portfolio/static/media/digitizer.63028b80.woff"
+  },
+  {
+    "revision": "39266e846a1a9f7986fbe676acba862c",
+    "url": "/portfolio/static/media/me.39266e84.png"
+  },
+  {
+    "revision": "9c43960ec2f45bd1ad5f6cf667e7ffaf",
+    "url": "/portfolio/static/media/wam.9c43960e.png"
+  }
+]);
