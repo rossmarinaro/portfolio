bb6e16a14e3c5e57930a038f65e2485ed677ae86
Ross <44373734+rossmarinaro@users.noreply.github.com>
2021-06-09 10:37:07 -0400

    Add files via upload

---
+++ precache-manifest.60309269bf044edfb9c29a901ae2aba9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4221267141e588e9151ee6c19b538948",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa1422aefe7be96dea96",
+    "url": "/portfolio/static/css/main.c4265242.chunk.css"
+  },
+  {
+    "revision": "6b71c5a5abf550e39f50",
+    "url": "/portfolio/static/js/2.7b501cc8.chunk.js"
+  },
+  {
+    "revision": "fa1422aefe7be96dea96",
+    "url": "/portfolio/static/js/main.ae866441.chunk.js"
+  },
+  {
+    "revision": "1a4d47e7058fefdd3b2f",
+    "url": "/portfolio/static/js/runtime-main.fe203139.js"
+  },
+  {
+    "revision": "617ae92ed8b2fdecca9cd779852b8549",
+    "url": "/portfolio/static/media/1.617ae92e.png"
+  },
+  {
+    "revision": "7b01625df2116a7887c6567591bde5f6",
+    "url": "/portfolio/static/media/1.7b01625d.png"
+  },
+  {
+    "revision": "107725f0c272f83dcf02680b21aa28f3",
+    "url": "/portfolio/static/media/2.107725f0.png"
+  },
+  {
+    "revision": "ee397d399d719226336f92c81d2fa6be",
+    "url": "/portfolio/static/media/2.ee397d39.png"
+  },
+  {
+    "revision": "6d45ab55f36813c8f2ca23a7d110d7c6",
+    "url": "/portfolio/static/media/3.6d45ab55.png"
+  },
+  {
+    "revision": "6ef237553f88ff499845c3fbca9ec330",
+    "url": "/portfolio/static/media/3.6ef23755.png"
+  },
+  {
+    "revision": "f58b24584160866c343a6bc5f2a2b33c",
+    "url": "/portfolio/static/media/4.f58b2458.png"
+  },
+  {
+    "revision": "42e56004660c7301f4834fb905529414",
+    "url": "/portfolio/static/media/amnesiapsyduck.42e56004.svg"
+  },
+  {
+    "revision": "43b0d939d0829db1aa1938cc7b3dab14",
+    "url": "/portfolio/static/media/bangers.43b0d939.woff"
+  },
+  {
+    "revision": "2c0bee9d3e4906dab18b37594f453ae7",
+    "url": "/portfolio/static/media/bigpoeghost.2c0bee9d.svg"
+  },
+  {
+    "revision": "a7b6faee4ebcf24bba3b573682b81997",
+    "url": "/portfolio/static/media/carrot.a7b6faee.svg"
+  },
+  {
+    "revision": "63028b801b021d4f09bfbeac91c1d6ea",
+    "url": "/portfolio/static/media/digitizer.63028b80.woff"
+  },
+  {
+    "revision": "7dd312aa4d47b8372610f74e4f281ed7",
+    "url": "/portfolio/static/media/ickis.7dd312aa.svg"
+  },
+  {
+    "revision": "39266e846a1a9f7986fbe676acba862c",
+    "url": "/portfolio/static/media/me.39266e84.png"
+  },
+  {
+    "revision": "0f52c7c9630720f83b43b92bae6c2d7e",
+    "url": "/portfolio/static/media/pin.0f52c7c9.svg"
+  },
+  {
+    "revision": "2326e509fc3516c511f2c86da9b2ecac",
+    "url": "/portfolio/static/media/spaghetti.2326e509.svg"
+  },
+  {
+    "revision": "d728bb0580602ded03060c0003b55e4e",
+    "url": "/portfolio/static/media/wam.d728bb05.svg"
+  }
+]);
