4709136d0e63c19db2b0bf5f797b591ab350381b
Ross <44373734+rossmarinaro@users.noreply.github.com>
2023-06-20 15:21:15 -0400

    Add files via upload

---
+++ precache-manifest.0d065225653798329e8543d1a8d9ebb2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2484901b51e45cd89067a73ce6aab9f5",
+    "url": "./portfolio/index.html"
+  },
+  {
+    "revision": "f432403fbc9b0e1229dc",
+    "url": "./portfolio/static/css/main.29726cc5.chunk.css"
+  },
+  {
+    "revision": "426c2da4b6a32d0e5fbe",
+    "url": "./portfolio/static/js/2.b9f59766.chunk.js"
+  },
+  {
+    "revision": "f432403fbc9b0e1229dc",
+    "url": "./portfolio/static/js/main.b88bc3b1.chunk.js"
+  },
+  {
+    "revision": "75cffdf6f7105c409c30",
+    "url": "./portfolio/static/js/runtime-main.c7a196a4.js"
+  },
+  {
+    "revision": "617ae92ed8b2fdecca9cd779852b8549",
+    "url": "./portfolio/static/media/1.617ae92e.png"
+  },
+  {
+    "revision": "7b01625df2116a7887c6567591bde5f6",
+    "url": "./portfolio/static/media/1.7b01625d.png"
+  },
+  {
+    "revision": "107725f0c272f83dcf02680b21aa28f3",
+    "url": "./portfolio/static/media/2.107725f0.png"
+  },
+  {
+    "revision": "ee397d399d719226336f92c81d2fa6be",
+    "url": "./portfolio/static/media/2.ee397d39.png"
+  },
+  {
+    "revision": "6d45ab55f36813c8f2ca23a7d110d7c6",
+    "url": "./portfolio/static/media/3.6d45ab55.png"
+  },
+  {
+    "revision": "6ef237553f88ff499845c3fbca9ec330",
+    "url": "./portfolio/static/media/3.6ef23755.png"
+  },
+  {
+    "revision": "f58b24584160866c343a6bc5f2a2b33c",
+    "url": "./portfolio/static/media/4.f58b2458.png"
+  },
+  {
+    "revision": "42e56004660c7301f4834fb905529414",
+    "url": "./portfolio/static/media/amnesiapsyduck.42e56004.svg"
+  },
+  {
+    "revision": "43b0d939d0829db1aa1938cc7b3dab14",
+    "url": "./portfolio/static/media/bangers.43b0d939.woff"
+  },
+  {
+    "revision": "2c0bee9d3e4906dab18b37594f453ae7",
+    "url": "./portfolio/static/media/bigpoeghost.2c0bee9d.svg"
+  },
+  {
+    "revision": "b4e68370477d1100f2670725220a66ec",
+    "url": "./portfolio/static/media/bounty hunter.b4e68370.png"
+  },
+  {
+    "revision": "a7b6faee4ebcf24bba3b573682b81997",
+    "url": "./portfolio/static/media/carrot.a7b6faee.svg"
+  },
+  {
+    "revision": "602525e17d8f4f2b381607256c1eb60f",
+    "url": "./portfolio/static/media/collie flower.602525e1.png"
+  },
+  {
+    "revision": "f22ca140c951ed2971014e0869e47416",
+    "url": "./portfolio/static/media/demo.f22ca140.svg"
+  },
+  {
+    "revision": "63028b801b021d4f09bfbeac91c1d6ea",
+    "url": "./portfolio/static/media/digitizer.63028b80.woff"
+  },
+  {
+    "revision": "7dd312aa4d47b8372610f74e4f281ed7",
+    "url": "./portfolio/static/media/ickis.7dd312aa.svg"
+  },
+  {
+    "revision": "46c408a6228a711729f3c12949126c80",
+    "url": "./portfolio/static/media/juicemafia.46c408a6.png"
+  },
+  {
+    "revision": "cf58e09c0cadbeeb7e9225ca08f3fcf8",
+    "url": "./portfolio/static/media/karina_alexi.cf58e09c.png"
+  },
+  {
+    "revision": "729ded7c3f57bc2564b39b021046be41",
+    "url": "./portfolio/static/media/me.729ded7c.png"
+  },
+  {
+    "revision": "2495a92b9abcf3f7890489b997c95b82",
+    "url": "./portfolio/static/media/pin.2495a92b.svg"
+  },
+  {
+    "revision": "2326e509fc3516c511f2c86da9b2ecac",
+    "url": "./portfolio/static/media/spaghetti.2326e509.svg"
+  },
+  {
+    "revision": "ef35aec4e691ed3711b38394e78a2a45",
+    "url": "./portfolio/static/media/spaghyeti.ef35aec4.svg"
+  },
+  {
+    "revision": "63cc674f3bab17307d7234ffedadb5dd",
+    "url": "./portfolio/static/media/sv.63cc674f.svg"
+  },
+  {
+    "revision": "d728bb0580602ded03060c0003b55e4e",
+    "url": "./portfolio/static/media/wam.d728bb05.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.83154a1d14a0ba508122e07f58d5aa2d.js"
+  "./portfolio/precache-manifest.0d065225653798329e8543d1a8d9ebb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
